46d5fc0908ccb73f8056f86bd14af25f76e1d7b8
asosiymenu007 <asosiymenu007@gmail.com>
2024-01-24 01:40:41 +0500

    ja masala

---
+++ index.js
@@ -0,0 +1,110 @@
+//1-misol
+
+// let oy = 5;
+// let res = '';
+
+
+// switch (oy) {
+//     case 1:
+//         res = `Yanvar`
+//         break;
+//     case 2:
+//         res = `Fevral`
+//         break;
+//     case 3:
+//         res = `Mart`
+//         break;
+//     case 4:
+//         res = `Aprel`
+//         break;
+//     case 5:
+//         res = `May`
+//         break;
+//     case 6:
+//         res = `Iyun`
+//         break;
+//     case 7:
+//         res = `Iyul`
+//         break;
+//     case 8:
+//         res = `Avgust`
+//         break;
+//     case 9:
+//         res = `Sentabr`
+//         break;
+//     case 10:
+//         res = `Oktabr`
+//         break;
+//     case 11:
+//         res = `Noyabr`
+//         break;
+//     case 12:
+//         res = `Dekabr`
+//         break;
+
+//     default:
+//         res = `Bunday oy yoq`
+//         break;
+// }
+// console.log(res);
+
+
+
+//2-misol
+
+// let num = 3;
+// let res = ''
+
+// switch (num) {
+//     case 1:
+//         res = `Qish`;
+//         break;
+//     case 2:
+//         res = `Qish`;
+//         break;
+//     case 3:
+//         res = `Bahor`;
+//         break;
+//     case 4:
+//         res = `Bahor`;
+//         break;
+//     case 5:
+//         res = `Bahor`;
+//         break;
+//     case 6:
+//         res = `Yoz`;
+//         break;
+//     case 7:
+//         res = `Yoz`;
+//         break;
+//     case 8:
+//         res = `Yoz`;
+//         break;
+//     case 9:
+//         res = `Kuz`;
+//         break;
+//     case 10:
+//         res = `Kuz`;
+//         break;
+//     case 11:
+//         res = `kuz`;
+//         break;
+//     case 12:
+//         res = `Qish`;
+//         break;
+
+//     default:
+//         res = `Bunday fasl yoq`;
+//         break;
+// }
+// console.log(res);
+
+
+
+//4-misol
+// let n = 10; 
+// let sum = 0;
+// for (let i = 1; i <= n; i++) {
+//   sum += i;
+// }
+// console.log(`1 dan ${n} gacha barcha sonlar yig'indisi: ${sum}`);
